b6f51adcf29bed14f0a0322389bfb5090784cee2
MrExplode <me@sunstorm.rocks>
2024-09-23 18:38:04 +0200

    feat(lesson2): add correct reactivity

--- app/src/main/java/me/sunstorm/devapp/MainActivity.kt
+++ app/src/main/java/me/sunstorm/devapp/MainActivity.kt
@@ -12,6 +12,11 @@ import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -49,14 +54,24 @@ fun GreetingPreview() {
 fun DiceWithButtonAndImage(modifier: Modifier = Modifier
     .fillMaxSize()
     .wrapContentSize(Alignment.Center)) {
-    var result = 1
+    var result by remember {
+        mutableIntStateOf(1)
+    }
 
+    val imageResource = when(result) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
     Column(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Image(
-            painter = painterResource(R.drawable.dice_1),
+            painter = painterResource(imageResource),
             contentDescription = result.toString()
         )
         Spacer(modifier = Modifier.height(16.dp))
